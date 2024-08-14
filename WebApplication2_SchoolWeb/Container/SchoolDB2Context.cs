79f1c2174f3bbc628e18077e41e1c7877c9306f9
Mostafa Mohamed <mostafa.mohamed851@yahoo.com>
2024-08-14 23:58:20 +0200

    InitialCreation

---
+++ WebApplication2_SchoolWeb/Container/SchoolDB2Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2_SchoolWeb.Models;
+
+namespace WebApplication2_SchoolWeb.Container
+{
+    public class SchoolDB2Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-A1088VT\\ADMIN;Database=School2DB;User Id=sa;password=Admin123;Integrated security=True;Encrypt=False");
+
+        }
+
+        public virtual Student? students { get; set; }
+        public virtual Department? departments { get; set; }
+    }
+}
---
+++ WebApplication2_SchoolWeb/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2_SchoolWeb.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public virtual List<Student>? students { get; set; } = new List<Student>();
+    }
+}
